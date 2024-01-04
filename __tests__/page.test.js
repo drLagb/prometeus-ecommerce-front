8eb40c7d72cb0b1c4c6f924dfc798b92351010f8
drLagb <drlagb1703@gmail.com>
2024-01-04 15:26:54 -0500

    agregado jest

---
+++ __tests__/page.test.js
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import {Home} from '../app/ComponentePrueba'
+ 
+describe('Page', () => {
+  it('renders a heading', () => {
+    render(<Home />);
+
+    expect(2 + 2).toBe(4);
+  })
+})
